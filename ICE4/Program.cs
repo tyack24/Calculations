d57480e94e7ecfbc12f331ed73a98fdab25099bf
Tyler Ackerina (RIT Student) <tja4925@rit.edu>
2020-08-26 09:34:19 -0400

    Add project files.

---
+++ ICE4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ICE4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius = 20;
+            const int FREEZING_POINT = 32;
+            double fahrenheit = ((9 * celisus) / 5) + FREEZING_POINT;
+        }
+    }
+}
